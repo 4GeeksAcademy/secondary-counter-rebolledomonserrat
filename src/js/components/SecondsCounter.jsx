b397de30794388c02c026ceccd0c414d13d17d0c
monserratrebolledo <94879940+monserratrebolledo@users.noreply.github.com>
2025-07-16 22:25:57 +0000

    agrega src/js/components/SecondsCounter.jsx

--- src/js/components/SecondsCounter.jsx
+++ src/js/components/SecondsCounter.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { FaRegClock} from "react-icons/fa";
 
 const SecondsCounter = ({ seconds }) => {
   const formatNumber = (num) => num.toString().padStart(6, "0");
@@ -16,7 +17,21 @@ const SecondsCounter = ({ seconds }) => {
       fontSize: "2rem",
       fontFamily: "monospace",
     }}>
-      <span style={{ marginRight: "10px" }}>⏳</span>
+       <div style={{
+          width: "50px",
+          height: "70px",
+          background: "#333",
+          color: "white",
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          fontSize: "2rem",
+          margin: "5px",
+          borderRadius: "5px",
+          boxShadow: "2px 2px 5px rgba(255, 255, 255, 0.3)",
+          }}>
+           <FaRegClock />
+        </div>
       {formattedTime.map((digit, index) => (
         <div key={index} style={{
           width: "50px",
